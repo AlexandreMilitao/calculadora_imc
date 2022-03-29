d63a28240935f3a734863fa75b7acd5e0c849927
AleLevi <ale.levi@hotmail.com>
2022-03-29 11:06:06 -0300

    commit_function_calculate_and_controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, deprecated_member_use, sized_box_for_whitespace, unused_element
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, sized_box_for_whitespace, unused_element, implementation_imports, unnecessary_import, prefer_final_fields, unused_field
 
 import 'package:flutter/src/material/raised_button.dart';
 import 'package:flutter/material.dart';
@@ -21,9 +21,35 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
-  void _resetFilds() {
-    weightController.text = "";
-    heightController.text = "";
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc <= 18.5) {
+        _infoText = "Abaixo do Peso(IMC:${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc <= 24.9) {
+        _infoText = "Peso normal (IMC:${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 25 && imc <= 25.9) {
+        _infoText = "Sobrepeso (IMC:${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 30 && imc <= 34.9) {
+        _infoText = "Obesidade 1 (IMC:${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 35 && imc <= 39.9) {
+        _infoText = "Obesidade 2 (IMC:${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade 3 (IMC:${imc.toStringAsPrecision(4)})";
+      }
+    });
   }
 
   @override
@@ -34,7 +60,10 @@ class _HomeState extends State<Home> {
           centerTitle: true,
           backgroundColor: Colors.green,
           actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _resetFields,
+            )
           ],
         ),
         backgroundColor: Colors.white,
@@ -66,7 +95,7 @@ class _HomeState extends State<Home> {
             Container(
               height: 50.0,
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: _calculate, //chamando a função no botão
                 child: Text(
                   "Calcular",
                   style: TextStyle(
@@ -81,7 +110,7 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
             ),
             Text(
-              "Info",
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.green,
